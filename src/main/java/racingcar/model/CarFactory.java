e916d68c7954cec8573835380c24afbf297bed77
circlezero <wy129@naver.com>
2025-10-23 14:41:23 +0900

    feat(CarFactory) : 팩토리 객체 추가

---
+++ src/main/java/racingcar/model/CarFactory.java
@@ -0,0 +1,33 @@
+package racingcar.model;
+
+public class CarFactory {
+
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final String NAME_PATTERN = "[A-Za-z0-9 ]+";
+
+    public static Car of(String name) {
+        String normalizeName = normalizeName(name);
+        validateName(normalizeName);
+
+        return new Car(normalizeName);
+    }
+
+    private static String normalizeName(String name) {
+        if(name == null) {
+            return "";
+        }
+        return name.trim();
+    }
+
+    private static void validateName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("이름은 null이거나 공백일 수 없습니다.");
+        }
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름은 5자를 초과할 수 없습니다.");
+        }
+        if (!name.matches(NAME_PATTERN)) {
+            throw new IllegalArgumentException("영문/숫자/공백만 허용됩니다.");
+        }
+    }
+}
