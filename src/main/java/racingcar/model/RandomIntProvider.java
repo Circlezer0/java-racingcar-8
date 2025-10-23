bbea28ecb094d02bfc276061b597e04aed254cb5
circlezero <wy129@naver.com>
2025-10-23 17:18:02 +0900

    feat(RandomIntProvider) : 랜덤 정수 생성자 객체 구현

---
+++ src/main/java/racingcar/model/RandomIntProvider.java
@@ -0,0 +1,24 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomIntProvider implements IntProvider{
+    private final int min;
+    private final int max;
+
+    public RandomIntProvider(int min, int max) {
+        if(min > max) {
+            throw new IllegalArgumentException("최소값은 최대값보다 클 수 없습니다.");
+        }
+        if(min < 0) {
+            throw new IllegalArgumentException("최소값과 최대값은 음수일 수 없습니다.");
+        }
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public int nextInt() {
+        return Randoms.pickNumberInRange(min, max);
+    }
+}
