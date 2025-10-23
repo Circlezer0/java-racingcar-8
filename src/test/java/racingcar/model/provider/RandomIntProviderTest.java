de543092bac868abc0ff5028e5befce869447a3f
circlezero <wy129@naver.com>
2025-10-23 19:49:05 +0900

    test(RandomIntProviderTest) : DisplayName 추가

--- src/test/java/racingcar/model/provider/RandomIntProviderTest.java
+++ src/test/java/racingcar/model/provider/RandomIntProviderTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -15,17 +16,19 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class RandomIntProviderTest {
 
     @ParameterizedTest(name = "min={0}, max={1}")
+    @DisplayName("객체 정상 생성 테스트")
     @CsvSource({
             "0, 10",
             "5, 15"
     })
-    void 최소값_최대값_설정_정상(int min, int max) {
+    void createCarsTest(int min, int max) {
         // when & then
         assertDoesNotThrow(() -> new RandomIntProvider(min, max));
     }
 
     @Test
-    void 랜덤_정수_제공_테스트() {
+    @DisplayName("랜덤 정수 제공 테스트")
+    void provideRandomIntTest() {
         // given
         int min = 0, max = 10;
         int repeatCount = 100;
@@ -42,13 +45,14 @@ public class RandomIntProviderTest {
     }
 
     @ParameterizedTest(name = "min={0}, max={1}")
+    @DisplayName("최소값과 최대값이 같을 때 항상 그 값만 제공 테스트")
     @CsvSource({
             "0, 0",
             "1, 1",
             "5, 5",
             "10, 10"
     })
-    void 최소값_최대값_같을_때_항상_그_값만_제공(int min, int max) {
+    void sameRangeSameValueTest(int min, int max) {
         // given
         RandomIntProvider intProvider = new RandomIntProvider(min, max);
         int repeatCount = 10;
@@ -64,22 +68,24 @@ public class RandomIntProviderTest {
     }
 
     @ParameterizedTest(name = "min={0}, max={1}")
+    @DisplayName("최소값이 최대값보다 클 때 IllegalArgumentException")
     @CsvSource({
             "10, 5",
             "5, 4"
     })
-    void 최소값_최대값_설정_오류_IllegalArgumentException(int min, int max) {
+    void biggerMinThanMaxErrorTest(int min, int max) {
         // when & then
         assertThrows(IllegalArgumentException.class, () -> new RandomIntProvider(min, max));
     }
 
     @ParameterizedTest(name = "min={0}, max={1}")
+    @DisplayName("음수 최소값 또는 최대값 설정 시 IllegalArgumentException")
     @CsvSource({
             "-1, 5",
             "0, -10",
             "-5, -1"
     })
-    void 음수_최소값_최대값_설정_오류_IllegalArgumentException(int min, int max) {
+    void negativeRangeErrorTest(int min, int max) {
         // when & then
         assertThrows(IllegalArgumentException.class, () -> new RandomIntProvider(min, max));
     }
