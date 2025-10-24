b404ebbdb6604703e97b7d30607a26d960938ecd
circlezero <wy129@naver.com>
2025-10-24 22:39:23 +0900

    feat(RaceView) : 입출력 뷰 구현

---
+++ src/main/java/racingcar/view/RaceView.java
@@ -0,0 +1,61 @@
+package racingcar.view;
+
+import java.util.List;
+import racingcar.model.car.CarName;
+import racingcar.model.car.CarStatus;
+import racingcar.view.handler.InputHandler;
+import racingcar.view.handler.OutputHandler;
+
+public class RaceView {
+    private final InputHandler inputHandler;
+    private final OutputHandler outputHandler;
+
+    private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TRY_COUNT = "시도할 횟수는 몇 회인가요?";
+    private static final String ROUND_START_MESSAGE = "실행 결과";
+    private static final String OUTPUT_WINNER = "최종 우승자 : ";
+
+
+    public RaceView(InputHandler inputHandler, OutputHandler outputHandler) {
+        this.inputHandler = inputHandler;
+        this.outputHandler = outputHandler;
+    }
+
+    public String readCarNames() {
+        outputHandler.printLine(INPUT_CAR_NAMES);
+        return inputHandler.readLine();
+    }
+
+    public int readTryCount() {
+        outputHandler.printLine(INPUT_TRY_COUNT);
+        return inputHandler.readInt();
+    }
+
+    public void displayRoundStartMessage() {
+        outputHandler.printLine();
+        outputHandler.printLine(ROUND_START_MESSAGE);
+    }
+
+    public void displayRoundResult(List<CarStatus> carStatuses) {
+        carStatuses.stream()
+                .map(this::formatCarStatus)
+                .forEach(outputHandler::printLine);
+
+        outputHandler.printLine();
+    }
+
+    public void displayWinners(List<CarName> winnerNames) {
+        String formattedWinners = winnerNames.stream()
+                .map(CarName::name)
+                .reduce((a, b) -> a + ", " + b)
+                .orElse("");
+
+        outputHandler.printLine(OUTPUT_WINNER + formattedWinners);
+    }
+
+    private String formatCarStatus(CarStatus status) {
+        return status.name()
+                + " : "
+                + "-".repeat(Math.max(0, status.position()));
+    }
+}
