66afc24c9c7b47bc64de96ae3e652bff3d4d36be
circlezero <wy129@naver.com>
2025-10-24 22:38:50 +0900

    feat(OutputHandler) : 출력 핸들러 구현

---
+++ src/main/java/racingcar/view/handler/OutputHandler.java
@@ -0,0 +1,10 @@
+package racingcar.view.handler;
+
+public class OutputHandler {
+    public void printLine() {
+        System.out.println();
+    }
+    public void printLine(String message) {
+        System.out.println(message);
+    }
+}
