457b55496cd8715e0e5883646854ea4edabda907
circlezero <wy129@naver.com>
2025-10-24 22:42:11 +0900

    refactor(Application) : 각 객체 생성 및 의존성 주입, 컨트롤러 실행 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,47 @@
 package racingcar;
 
+import racingcar.controller.RaceController;
+import racingcar.model.provider.IntProvider;
+import racingcar.model.provider.RandomIntProvider;
+import racingcar.model.strategy.MoveStrategy;
+import racingcar.model.strategy.ThresholdMoveStrategy;
+import racingcar.service.ParseService;
+import racingcar.service.RaceService;
+import racingcar.view.RaceView;
+import racingcar.view.handler.InputHandler;
+import racingcar.view.handler.OutputHandler;
+
 public class Application {
+    private static final int RANDOM_MIN = 0;
+    private static final int RANDOM_MAX = 9;
+    private static final int MOVE_THRESHOLD = 4;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        RaceService raceService = createRaceService(RANDOM_MIN, RANDOM_MAX, MOVE_THRESHOLD);
+        RaceView raceView = createRaceView();
+
+        RaceController raceController = createRaceController(raceService, raceView);
+
+        raceController.run();
     }
+
+    private static RaceController createRaceController(RaceService raceService, RaceView raceView) {
+        ParseService parseService = new ParseService();
+
+        return new RaceController(raceService, parseService, raceView);
+    }
+
+    private static RaceService createRaceService(int randomMin, int randomMax, int moveThreshold) {
+        IntProvider intProvider = new RandomIntProvider(randomMin, randomMax);
+        MoveStrategy moveStrategy = new ThresholdMoveStrategy(intProvider, moveThreshold);
+        return new RaceService(moveStrategy);
+    }
+
+    private static RaceView createRaceView() {
+        InputHandler inputHandler = new InputHandler();
+        OutputHandler outputHandler = new OutputHandler();
+        return new RaceView(inputHandler, outputHandler);
+    }
+
 }
