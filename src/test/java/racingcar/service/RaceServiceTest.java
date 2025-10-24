54ba4c1ed0ea5707ad408f5b370b4dd557cd2cbe
circlezero <wy129@naver.com>
2025-10-24 21:27:38 +0900

    test(RaceServiceTest) : 경기 관리 객체 테스트코드 생성

---
+++ src/test/java/racingcar/service/RaceServiceTest.java
@@ -0,0 +1,138 @@
+package racingcar.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+
+import java.util.List;
+import org.assertj.core.groups.Tuple;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.model.car.CarName;
+import racingcar.model.car.CarStatus;
+import racingcar.model.car.Cars;
+import racingcar.model.car.CarsFactory;
+import racingcar.model.provider.IntProvider;
+import racingcar.model.provider.SequenceIntProvider;
+import racingcar.model.strategy.MoveStrategy;
+import racingcar.model.strategy.ThresholdMoveStrategy;
+
+public class RaceServiceTest {
+
+    Cars cars;
+    MoveStrategy moveStrategy;
+
+    @BeforeEach
+    void setUp() {
+        List<String> carNames = List.of("car1", "car2", "car3");
+        cars = CarsFactory.of(carNames);
+        IntProvider intProvider = new SequenceIntProvider(new int[]{
+                7, 1, 4,    // Round 1 (T F T)
+                0, 3, 9,    // Round 2 (F F T)
+                5, 2, 6,    // Round 3 (T F T)
+                3, 2, 1,    // Round 4 (F F F)
+                0, 9, 0     // Round 5 (F T F)
+                // Result : 2, 1, 3
+        });
+        moveStrategy = new ThresholdMoveStrategy(intProvider, 4);
+    }
+
+    @Test
+    @DisplayName("한 라운드 진행 테스트")
+    void playRoundTest() {
+        // given
+        RaceService raceService = new RaceService(moveStrategy);
+
+        // when
+        raceService.playRound(cars);
+
+        // then
+        Tuple[] expected = {
+                tuple("car1", 1),
+                tuple("car2", 0),
+                tuple("car3", 1)
+        };
+
+        assertThat(cars.getCarStatuses())
+                .extracting(CarStatus::name, CarStatus::position)
+                .containsExactly(expected);
+    }
+
+    @Test
+    @DisplayName("모든 라운드 진행 테스트")
+    void playAllRoundsTest() {
+        // given
+        RaceService raceService = new RaceService(moveStrategy);
+        int totalRounds = 5;
+
+        // when
+        for (int i = 0; i < totalRounds; i++) {
+            raceService.playRound(cars);
+        }
+
+        // then
+        Tuple[] expected = {
+                tuple("car1", 2),
+                tuple("car2", 1),
+                tuple("car3", 3)
+        };
+
+        assertThat(cars.getCarStatuses())
+                .extracting(CarStatus::name, CarStatus::position)
+                .containsExactly(expected);
+    }
+
+    @Test
+    @DisplayName("우승자 결정 테스트")
+    void getWinnersTest() {
+        // given
+        RaceService raceService = new RaceService(moveStrategy);
+        int totalRounds = 5;
+
+        // when
+        for (int i = 0; i < totalRounds; i++) {
+            raceService.playRound(cars);
+        }
+        List<CarName> winners = raceService.getWinners(cars);
+
+        // then
+        assertThat(winners)
+                .extracting(CarName::name)
+                .containsExactly("car3");
+    }
+
+    @Test
+    @DisplayName("Cars 객체 변경 테스트")
+    void changeCarsTest() {
+        // given
+        RaceService raceService = new RaceService(moveStrategy);
+        Cars newCars = CarsFactory.of(List.of("alpha", "beta"));
+
+        // when
+        raceService.playRound(cars);
+        raceService.playRound(cars);
+        raceService.playRound(cars);
+
+        raceService.playRound(newCars);
+        raceService.playRound(newCars);
+        raceService.playRound(newCars);
+
+        // then
+        Tuple[] expectedOldCars = {
+                tuple("car1", 2),
+                tuple("car2", 0),
+                tuple("car3", 3)
+        };
+        assertThat(cars.getCarStatuses())
+                .extracting(CarStatus::name, CarStatus::position)
+                .containsExactly(expectedOldCars);
+
+        Tuple[] expectedNewCars = {
+                tuple("alpha", 1),
+                tuple("beta", 0)
+        };
+        assertThat(newCars.getCarStatuses())
+                .extracting(CarStatus::name, CarStatus::position)
+                .containsExactly(expectedNewCars);
+    }
+}
