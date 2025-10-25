28ac641325ff1a814334127aafea472df2f8b076
circlezero <wy129@naver.com>
2025-10-26 02:52:21 +0900

    refactor(RaceRound) : 포멧팅 수정

--- src/main/java/racingcar/model/car/RaceRound.java
+++ src/main/java/racingcar/model/car/RaceRound.java
@@ -8,7 +8,11 @@ public record RaceRound(int round) {
     private static final int MAX_ROUND = 100;
 
     public RaceRound {
-        if (round < MIN_ROUND) throw new RaceException(RaceRoundErrorCode.NEGATIVE_ROUND);
-        if (round > MAX_ROUND) throw new RaceException(RaceRoundErrorCode.MAX_ROUND_EXCEEDED);
+        if (round < MIN_ROUND) {
+            throw new RaceException(RaceRoundErrorCode.NEGATIVE_ROUND);
+        }
+        if (round > MAX_ROUND) {
+            throw new RaceException(RaceRoundErrorCode.MAX_ROUND_EXCEEDED);
+        }
     }
 }
